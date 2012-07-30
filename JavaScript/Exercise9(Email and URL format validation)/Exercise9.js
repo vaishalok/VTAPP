c29d1a9544b90719998ee2dbe77e4b290b0c0ce2
Alok Vaish <vaish.alok16@gmail.com>
2012-07-30 23:08:01 +0530

    Javascript exercise 9 (Email and URL format validation) modified.

--- JavaScript/Exercise9(Email and URL format validation)/Exercise9.js
+++ JavaScript/Exercise9(Email and URL format validation)/Exercise9.js
@@ -1,61 +1,58 @@
-function validateForm() {
+var returnValue;
 
+function validateForm() {
+	returnValue = true;
 	var form = document.forms["registration"];
-	var returnValue = true;
 	
-	var loginId = form ["loginId"].value;
-	if (loginId==null || loginId=="") {
-		alert("Login Id can't be empty.");
-		returnValue = false;
+	var fields = document.getElementsByTagName("input");
+	for (var i=0; i < fields.length; i++) {
+		if (fields[i].type == 'text') {
+			isBlank(fields[i].name, fields[i].value);
+		}
 	}
 	
-	var email = form ["email"].value;
-	if (email==null || email=="" || email.trim() =="") {
-		alert("Email can't be empty.");
-		returnValue = false;
-	} else {
-		var trimedEmail = email.trim();
-		var emailRegExp = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/);
-		if(!emailRegExp.test(trimedEmail)) {
-			alert("Please enter valid email.");
-			returnValue = false;
-		}	
-	}
-	
-	var name = form ["name"].value;
-	if (name==null || name=="") {
-		alert("Name can't be empty.");
-		returnValue = false;
-	}
-	
-	var homePage = form ["homePage"].value;
-	if (homePage==null || homePage=="" || homePage.trim() == "") {
-		alert("Home Page can't be empty.");
-		returnValue = false;
-	} else {
-		var trimedHomePage = homePage.trim();
-		var URLRegExp = new RegExp(/^([a-zA-Z]*:\/\/)?[a-zA-Z0-9\-\._]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)\/?$/);
-		if(!URLRegExp.test(trimedHomePage)) {
-			alert("Please enter valid Home Page adddress.");
-			returnValue = false;
-		}	
-	}
-	
-	var aboutMe = form ["aboutMe"].value;
-	if (aboutMe==null || aboutMe=="") {
-		alert("About Me can't be empty.");
-		returnValue = false;
-	} else if(aboutMe.length < 50) {
+	//About Me
+	var aboutMe = form ["About Me"].value;
+	if(!isBlank("About Me", aboutMe) && aboutMe.length < 50) {
 		alert("Minimum 50 characters required for about me.");
 		returnValue = false;
 	}
 	
-	var receiveNotification = form ["receiveNotification"].checked;
+	//receive Notification
+	var receiveNotification = form ["Receive Notification"].checked;
 	if (!receiveNotification) {
 		alert("Please check the Receive Notification checkbox.");
 		returnValue = false;
 	}
+		
+	if(!returnValue) {
+		return returnValue;
+	}	
+	
+	//Email
+	var email = form ["Email"].value;
+	var trimedEmail = email.trim();
+	var emailRegExp = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/);
+	if(!emailRegExp.test(trimedEmail)) {
+		alert("Please enter valid email.");
+		returnValue = false;
+	}	
+	
+	//HomePage
+	var homePage = form ["Home Page"].value;
+	var trimedHomePage = homePage.trim();
+	var URLRegExp = new RegExp(/^([a-zA-Z]*:\/\/)?[a-zA-Z0-9\-\._]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)\/?$/);
+	if(!URLRegExp.test(trimedHomePage)) {
+		alert("Please enter valid Home Page adddress.");
+		returnValue = false;
+	}	
 	
 	return returnValue;
-	
+}
+
+function isBlank(fieldName, fieldValue) {
+	if (fieldValue==null || fieldValue.trim()=="") {
+		alert(fieldName + " can't be empty.");
+		returnValue = false;
+	}
 }
